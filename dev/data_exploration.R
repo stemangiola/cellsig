3c2c9b43cc50fb0c17f7bb175ec0e34bccba0af7
jianwu1 <wujian0507.jw@gmail.com>
2020-08-08 14:38:10 +1000

    loose clusters

--- dev/data_exploration.R
+++ dev/data_exploration.R
@@ -35,59 +35,59 @@ tt <-
   mutate(data = future_map(data, ~ scale_abundance(.x)))
 
 
-tt_naive <-  
-  tt %>%
-
-  # Scale and reduce dimensions
-  mutate(data = future_map(
-    data,
-    ~ .x %>%
-      scale_abundance() %>%
-      reduce_dimensions(method="MDS") %>%
-      reduce_dimensions(method="PCA") %>%
-      reduce_dimensions(method="tSNE")
-  )) %>%
-
-  # Cluster
-  mutate(data = future_map(data,~ cluster_elements(.x, method="SNN")))
-
-
-counts_imm_epi_naive <-
-  tt %>%
-
-  # Investigate one level
-  filter(level==1) %>%
-
-  # investigate one cell type pair
-  mutate(comparison_data = map(
-    data,
-    ~ .x %>%
-      filter(cell_type %in% c("immune_cell", "epithelial")) %>%
-      mutate(cell_type = as.character(cell_type) )
-    )) %>%
-
-  #test. We run on the two populations but we select data for all populations
-  mutate(markers = map(
-    comparison_data,
-    ~ .x %>%
-      test_differential_abundance( ~ cell_type, action="only")
-    )) %>%
-
-  # Add marker info to original data
-  mutate(data = map2(data, markers, ~ left_join(.x, .y))) %>%
-  select(-comparison_data, - markers) %>%
-  unnest(data) %>%
-
-  # Nest
-  nest_subset(data = -symbol) %>%
-
-  # Select markers
-  filter(FDR < 0.05 & abs(logFC) > 2) %>%
-  filter(logCPM > mean(logCPM)) %>%
-  arrange(logFC %>% desc()) %>%
-  slice(1:10) %>%
-
-  unnest(data)
+# tt_naive <-  
+#   tt %>%
+# 
+#   # Scale and reduce dimensions
+#   mutate(data = future_map(
+#     data,
+#     ~ .x %>%
+#       scale_abundance() %>%
+#       reduce_dimensions(method="MDS") %>%
+#       reduce_dimensions(method="PCA") %>%
+#       reduce_dimensions(method="tSNE")
+#   )) %>%
+# 
+#   # Cluster
+#   mutate(data = future_map(data,~ cluster_elements(.x, method="SNN")))
+# 
+# 
+# counts_imm_epi_naive <-
+#   tt %>%
+# 
+#   # Investigate one level
+#   filter(level==1) %>%
+# 
+#   # investigate one cell type pair
+#   mutate(comparison_data = map(
+#     data,
+#     ~ .x %>%
+#       filter(cell_type %in% c("immune_cell", "epithelial")) %>%
+#       mutate(cell_type = as.character(cell_type) )
+#     )) %>%
+# 
+#   #test. We run on the two populations but we select data for all populations
+#   mutate(markers = map(
+#     comparison_data,
+#     ~ .x %>%
+#       test_differential_abundance( ~ cell_type, action="only")
+#     )) %>%
+# 
+#   # Add marker info to original data
+#   mutate(data = map2(data, markers, ~ left_join(.x, .y))) %>%
+#   select(-comparison_data, - markers) %>%
+#   unnest(data) %>%
+# 
+#   # Nest
+#   nest_subset(data = -symbol) %>%
+# 
+#   # Select markers
+#   filter(FDR < 0.05 & abs(logFC) > 2) %>%
+#   filter(logCPM > mean(logCPM)) %>%
+#   arrange(logFC %>% desc()) %>%
+#   slice(1:10) %>%
+# 
+#   unnest(data)
 
 ######################################################################################################
 
@@ -131,6 +131,8 @@ cell_sig <- function(input, pair) {
     unnest(data)
 }
 
+
+
 # extract top10 differentially expressed genes from 6 cell type pairs in level 1
 counts_imm_epi_de <- cell_sig(tt, c("immune_cell", "epithelial"))
 counts_imm_endo_de <- cell_sig(tt, c("immune_cell", "endothelial"))
@@ -139,15 +141,20 @@ counts_epi_endo_de <- cell_sig(tt, c("epithelial", "endothelial"))
 counts_epi_fib_de <- cell_sig(tt, c("epithelial", "fibroblast"))
 counts_endo_fib_de <- cell_sig(tt, c("endothelial", "fibroblast"))
 
+
+
 # collage all the differentially expressed genes between cell types
 sig_level1 <- bind_rows(counts_imm_epi_de, counts_imm_endo_de, counts_imm_fib_de, 
                counts_epi_endo_de, counts_epi_fib_de, counts_endo_fib_de) %>%
-  tidybulk(sample, symbol, count) %>% 
-  select(sample, symbol, count, count_scaled, cell_type) %>%
+  tidybulk(sample, symbol, count_scaled) %>% 
+  select(sample, symbol, count_scaled, cell_type) %>%
   
   # remove duplicate genes that arise during pairwise comparison and reduce dimensions
   distinct() %>%
+  # group_by(cell_type) %>% 
   reduce_dimensions(sample, symbol, count_scaled,  method = "PCA")
+
+
 
 # plot PCA 
 sig_level1 %>% 
