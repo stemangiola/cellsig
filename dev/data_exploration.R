d29d3a15d031a425e7b670e19dfdb499c032fb43
stemangiola <mangiolastefano@gmail.com>
2020-08-10 16:38:52 +1000

    fix marker selection

--- dev/data_exploration.R
+++ dev/data_exploration.R
@@ -15,6 +15,10 @@ library(tidybulk)
 
 load("data/counts.rda")
 
+
+
+
+# Setup data frame
 tt <- 
   
   # Load dataset
@@ -34,6 +38,7 @@ tt <-
   # Scale for future PCA plotting
   mutate(data = future_map(data, ~ scale_abundance(.x)))
 
+# No markers
 tt_naive <-  
   tt %>%
 
@@ -51,171 +56,98 @@ tt_naive <-
   mutate(data = future_map(data,~ cluster_elements(.x, method="SNN")))
 
 
-counts_endo_epi_naive <-
+# Functions
+get_constrasts_from_df = function(.data){
+  .data %>% 
+    
+    distinct(cell_type) %>% 
+    
+    # Permute
+    mutate(cell_type2 = cell_type) %>% 
+    expand(cell_type, cell_type2) %>% 
+    filter(cell_type != cell_type2) %>% 
+    
+    # Create contrasts
+    mutate(contrast = sprintf("cell_type%s - cell_type%s", cell_type, cell_type2)) %>%
+    pull(contrast)
+  
+}
+
+select_markers_for_each_contrast = function(.data){
+  .data %>%
+    
+    # Group by contrast. Comparisons both ways.
+    pivot_longer(
+      cols = contains("___"),
+      names_to = c("stats", "contrast"), 
+      values_to = ".value", 
+      names_sep="___"
+    ) %>% 
+    
+    # Markers selection
+    nest(stat_df = -contrast) %>%
+    
+    # Reshape inside each contrast
+    mutate(stat_df = map(stat_df, ~.x %>% pivot_wider(names_from = stats, values_from = .value))) %>%
+    
+    # Rank
+    mutate(stat_df = map(stat_df, ~.x %>%
+                           filter(FDR < 0.05 & abs(logFC) > 2) %>%
+                           filter(logCPM > mean(logCPM)) %>%
+                           arrange(logFC %>% desc()) %>%
+                           slice(1:10)        
+    )) %>%
+    unnest(stat_df)
+}
+
+# Level 1
+all_contrasts <-
   tt %>%
 
   # Investigate one level
   filter(level==1) %>%
 
-  # investigate one cell type pair
-  mutate(comparison_data = map(
-    data,
-    ~ .x %>%
-      filter(cell_type %in% c("endothelial", "epithelial")) %>%
-      mutate(cell_type = as.character(cell_type) )
-    )) %>%
-
-  #test. We run on the two populations but we select data for all populations
+  # Differential transcription
   mutate(markers = map(
-    comparison_data,
-    ~ .x %>%
-      
-      # Differential transcription
-      test_differential_abundance(
+    data,
+    ~ test_differential_abundance(.x,
         ~ 0 + cell_type, 
-        .contrasts = c(
-          "cell_typeendothelial - cell_typeepithelial", 
-          "cell_typeepithelial - cell_typeendothelial"
-        ),
+        .contrasts = get_constrasts_from_df(.x),
         action="only"
       ) 
     
   )) %>%
   
   # Select rank from each contrast
-  mutate(markers = map(
-    markers, ~ .x %>%
-      
-      # Group by contrast. Comparisons both ways.
-      # This results in for example
-      #
-      # A tibble: 1 x 2
-      # `cell_typeendothelial - cell_typeepithelial` `cell_typeepithelial - cell_typeendothelial`
-      # <list>                                       <list>                        
-      # 
-      pivot_longer(
-        cols = contains("___"),
-        names_to = c("stats", "contrast"), 
-        values_to = ".value", 
-        names_sep="___"
-      ) %>% 
-      
-      # Markers selection
-      nest(stat_df = -contrast) %>%
-      
-      # Reshape inside each contrast
-      mutate(stat_df = map(stat_df, ~.x %>% pivot_wider(names_from = stats, values_from = .value))) %>%
-      
-      # Rank
-      mutate(stat_df = map(stat_df, ~.x %>%
-                             filter(FDR < 0.05 & abs(logFC) > 2) %>%
-                             filter(logCPM > mean(logCPM)) %>%
-                             arrange(logFC %>% desc()) %>%
-                             slice(1:10)        
-      )) %>%
-      unnest(stat_df)
-  )) %>%
+  mutate(markers = map( markers, ~ .x %>% select_markers_for_each_contrast)) %>%
 
   # Add marker info to original data
   mutate(markers = map2(markers, data, ~ left_join(.x, .y))) %>%
-  select(-comparison_data, - data) %>%
-  unnest(markers) 
-
-
-######################################################################################################
-
-# cell_sig() extracts top10 differentially expressed genes from different cell type pairs
-
-cell_sig <- function(input, pair) {
+  select(markers) %>%
+  unnest(markers) %>%
   
-  input %>%
-    
-    # Investigate one level
-    filter(level==1) %>%
-    
-    # investigate one cell type pair
-    mutate(comparison_data = map(
-      data,
-      ~ .x %>%
-        filter(cell_type %in% pair) %>%
-        mutate(cell_type = as.character(cell_type) )
-    )) %>%
-    
-    #test. We run on the two populations but we select data for all populations
-    mutate(markers = map(
-      comparison_data,
-      ~ .x %>%
-        test_differential_abundance( ~ cell_type, sample, symbol, count, action="only")
-    )) %>%
-    
-    # Add marker info to original data
-    mutate(data = map2(data, markers, ~ left_join(.x, .y))) %>%
-    select(-comparison_data, - markers) %>%
-    unnest(data) %>%
-    
-    # Nest
-    nest_subset(data = -symbol) %>%
-    
-    # Select markers
-    filter(FDR < 0.05 & logFC > 2) %>%
-    filter(logCPM > mean(logCPM)) %>%
-    arrange(logFC %>% desc()) %>%
-    slice(1:10) %>%
-    unnest(data)
-}
-
-
-
-# extract top10 differentially expressed genes from 6 cell type pairs in level 1
-counts_imm_epi_de <- cell_sig(tt, c("immune_cell", "epithelial"))
-counts_imm_endo_de <- cell_sig(tt, c("immune_cell", "endothelial"))
-counts_imm_fib_de <- cell_sig(tt, c("immune_cell", "fibroblast"))
-counts_epi_endo_de <- cell_sig(tt, c("epithelial", "endothelial"))
-counts_epi_fib_de <- cell_sig(tt, c("epithelial", "fibroblast"))
-counts_endo_fib_de <- cell_sig(tt, c("endothelial", "fibroblast"))
-
-# permutation
-counts_epi_imm_de <- cell_sig(tt, c("epithelial", "immune_cell"))
-counts_endo_imm_de <- cell_sig(tt, c("endothelial", "immune_cell"))
-counts_fib_imm_de <- cell_sig(tt, c("fibroblast", "immune_cell"))
-counts_endo_epi_de <- cell_sig(tt, c("endothelial", "epithelial"))
-counts_fib_epi_de <- cell_sig(tt, c("fibroblast", "epithelial"))
-counts_fib_endo_de <- cell_sig(tt, c("fibroblast", "endothelial"))
-
-# collage all the differentially expressed genes between cell types
-sig_level1 <- bind_rows(counts_imm_epi_de, counts_imm_endo_de, counts_imm_fib_de, 
-               counts_epi_endo_de, counts_epi_fib_de, counts_endo_fib_de,
-               counts_epi_imm_de, counts_endo_imm_de, counts_fib_imm_de,
-               counts_endo_epi_de, counts_fib_epi_de, counts_fib_endo_de) %>%
-  tidybulk(sample, symbol, count_scaled) %>% 
-  select(sample, symbol, count_scaled, cell_type) %>%
+  # make contrasts pretty
+  mutate(contrast_pretty = str_replace(contrast, "cell_type", "") %>% str_replace("cell_type", "")) 
   
-  # remove duplicate genes that arise during pairwise comparison and reduce dimensions
-  distinct() %>%
-  # group_by(cell_type) %>% 
-  reduce_dimensions(sample, symbol, count_scaled,  method = "PCA")
 
+# Plot Markers
+all_contrasts %>% 
+  ggplot(aes(cell_type, count_scaled + 1, color=contrast_pretty)) + 
+  geom_point(size = 0.5) + 
+  facet_wrap(~contrast_pretty + symbol, scale="free_y") + 
+  scale_y_log10() +
+  theme_bw() +
+  theme(
+    text = element_text(size=6), 
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)
+  ) 
 
-
-# plot PCA 
-sig_level1 %>% 
-  pivot_sample(sample) %>% 
+# Plot PCA
+all_contrasts %>%
+  distinct(sample, symbol, count_scaled,cell_type) %>%
+  nanny::reduce_dimensions(sample, symbol, count_scaled,  method = "PCA", action="get", transform = log1p) %>%
   ggplot(aes(x = PC1, y = PC2, colour = cell_type)) + 
   geom_point() +
   theme_bw()
 
 
-# Compare marker gene counts distribution
-sig_level1 %>% 
-  ggplot(aes(x = cell_type, y = log(count_scaled + 1), colour = cell_type)) +
-  geom_boxplot() +
-  geom_jitter(aes(alpha=0.01)) +
-  facet_wrap(~symbol) +
-  theme(axis.text.x = element_text(angle = 45))
-
-# marker gene boxplot between endothelial and epithelial cells
-counts_epi_endo_de %>% 
-  ggplot(aes(x = symbol, y = log(count_scaled + 1), colour = cell_type)) +
-  geom_boxplot() +
-  geom_point(aes(alpha = 0.01)) +
-  theme(axis.text.x = element_text(angle = 45))
