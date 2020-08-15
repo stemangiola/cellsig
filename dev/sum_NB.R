4fee6bbb16e55098bd95baf626721e35888916b5
stemangiola <mangiolastefano@gmail.com>
2020-08-15 15:10:02 +1000

    managed to mle a compound gamma!

--- dev/sum_NB.R
+++ dev/sum_NB.R
@@ -1,51 +1,83 @@
+library(tidyverse)
+
+# Number of potential cells This number is relative and does not really matter
 n = 1000
-prop = c(0.6, 0.3, 0.1)
+
+# Proportion of the 3 cell type in evey sample/person
+prop = c(0.9, 0.05, 0.05)
+
+# Parameters
 mu = c(20, 200, 1000)
-sigma = exp(c(1.3420415 + log(mu) * -0.3386389))
+sigma = c(5, 25, 150)
+
+# Reparametrisation
+rgamma_rep = function(n, mu, phi){
+  
+  phi = phi * mu
+  
+  alpha <- mu * mu / phi; 
+  beta <- mu / phi;
+  
+  rgamma(n, shape = alpha, rate = beta)
+}
 
 
 mix_df = 
-  tibble(
-    prop = prop,
-    mu = mu
-  ) %>%
-  mutate(
-    population = 1:n(),
-    sigma = sigma
-  ) %>%
   
+  # Set up dataframe
+  tibble(  prop = prop,  mu = mu  ) %>%
+  mutate(  population = 1:n(),  sigma = sigma ) %>%
+  mutate(n = 1000) %>%
   
-  mutate(samples = map(population, ~ 1:10000)) %>%
-  unnest(samples) %>%
-  mutate(draws = pmap(list(mu, sigma, prop), ~ rnbinom(n * ..3, mu = ..1, size = ..2))) %>%
-  unnest(draws) %>%
-  nest(draws = -samples) %>%
-  mutate(mix = map_dbl(draws, ~ sum(.x)))
+  # Create 1000 samples/people
+  mutate(sample = map(population, ~ 1:1000)) %>%
+  unnest(sample) %>%
   
+  # Draw the abundance expected value for each sample/person for each cell type 
+  mutate(gamma_draw = rgamma_rep(n(), mu, sigma)) %>%
   
-  mutate(draws = map2(
-    mu, sigma, 
-    ~ rnbinom(10000, mu = .x, size = .y) %>% 
-      as_tibble() %>%
-      rownames_to_column(var = "draw")
-  )) %>%
-  unnest(draws) %>%
-  nest(data = -draw) %>%
-  mutate(mix = map_dbl(data, ~ sum(.x$value * .x$prop))) %>%
+  # Generate trancripts for 1000 cells from each cell type as reference, and sum because it is what we observe through sequencing
+  mutate(sum_cell_pure = map2_int(gamma_draw, n, ~ rpois(.y, .x) %>% sum)) %>%
   
-  # Print parameters
+  # Generate transcripts for 1000 total cells, divided among 3 cell types according to proportions, and sum the total gene abundance
+  mutate(sum_cell_in_mix = map2_int(gamma_draw, n*prop, ~ rpois(.y, .x) %>% sum)) %>%
+  nest(sample_df = -sample) %>%
+  
+  # Sum all outputs for each sample/person
+  mutate(mix_gamma_draw = map_dbl(sample_df, ~ sum(.x$gamma_draw))) %>%
+  mutate(mix = map_dbl(sample_df, ~ sum(.x$sum_cell_in_mix))) %>%
+
+  # Print estimated parameters of the mix
   {
     fitdistrplus::fitdist(data = as.integer((.)$mix), distr = "nbinom", method = "mle") %>%
       print();
     (.)
+  } %>%
+  
+  # Plot
+  {
+    ((.) %>% unnest( sample_df) %>%
+       ggplot(aes(mix)) + 
+       geom_density() + 
+       geom_density(aes(sum_cell_pure, color=as.factor(population))) +
+       scale_x_log10() +
+       ggtitle("the pure components and resulting mix in black")) %>%
+      plot()
+    
+    ((.) %>% unnest( sample_df) %>%
+      ggplot(aes(mix)) + 
+      geom_histogram() + 
+       geom_histogram(aes(sum_cell_in_mix, color=as.factor(population)), alpha=0.2) +
+      #geom_density() +
+      scale_x_log10() +
+       ggtitle("sum of the actual mixes")) %>%
+      plot()
+    (.)
+    
   }
 
 
-mix_df %>%
-  ggplot(aes(mix)) + 
-  geom_density() + 
-  geom_density(aes(value, color=population, data = mix_df %>% unnest(data))) +
-  scale_x_log10()
+
 
 
  gamlss::fitDist(as.integer(mix_df$mix), k = 2, type = "realplus", trace = FALSE, try.gamlss = TRUE)
@@ -70,7 +102,6 @@ sum_NB = function(lambda_mat, sigma_mat, prop_mat){
   
 sum_NB(matrix(mu), matrix(sigma), matrix(prop, nrow = 1))
 
-
 neg_binomial_sum_moments = function( mus, phis) {
   mu_approx = sum(mus)
   
@@ -85,3 +116,21 @@ neg_binomial_sum_moments = function( mus, phis) {
 
 neg_binomial_sum_moments(mu, sigma)
            
+
+LL = function(a1, a2, a3, b1, b2, b3){
+  -sum(
+    log(
+    .Call("_coga_dcoga_approx", PACKAGE = "coga", x, c(a1, a2, a3), c(b1, b2, b3)/100)
+    )
+  )
+}
+
+x <- mix_df$mix_gamma_draw
+
+mle(
+  minuslogl = LL, 
+  start = list(a1 = 4, a2 = 8, a3 =6, b1 = 20, b2 =4, b3 = 0.6),
+  lower = c( 0, 0, 0, 0, 0, 0),
+  upper = c(Inf, Inf, Inf, Inf, Inf, Inf),
+  method = "L-BFGS-B"
+)
