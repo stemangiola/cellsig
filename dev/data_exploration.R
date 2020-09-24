9b1b8b97fb05019ab61cf93313d60b9e4cedb844
jianwu1 <wujian0507.jw@gmail.com>
2020-09-24 10:59:22 +1000

    updated on 24/09/2020

--- dev/data_exploration.R
+++ dev/data_exploration.R
@@ -15,6 +15,8 @@ library(tidybulk)
 library(cluster)
 library(proxy)
 library(factoextra)
+library(stringr)
+
 load("data/counts.rda")
 
 
@@ -55,7 +57,14 @@ tt_naive <-
   )) %>%
 
   # Cluster
-  mutate(data = future_map(data,~ cluster_elements(.x, method="SNN")))
+  mutate(data = future_map(data, ~ cluster_elements(.x, method="SNN")))
+
+
+# For debugging you have 2 options
+## call debugonce(get_constrasts_from_df), run the function that calls this function, which is constrast(tt), 
+## when get_constrasts_from_df is called, you can just run any line to see the output
+## insert "browser()" inside the function, load the function, run the function
+
 
 
 # Functions
@@ -77,7 +86,7 @@ get_constrasts_from_df = function(.data){
 }
 
 ## 2
-select_markers_for_each_contrast = function(.data, sigsize){
+select_markers_for_each_contrast = function(.data, contrast_size){
   .data %>%
     
     # Group by contrast. Comparisons both ways.
@@ -99,13 +108,43 @@ select_markers_for_each_contrast = function(.data, sigsize){
                            filter(FDR < 0.05 & logFC > 2) %>%
                            filter(logCPM > mean(logCPM)) %>%
                            arrange(logFC %>% desc()) %>%
-                           slice(1:sigsize)     
+                           slice(1:contrast_size)     
     )) %>%
     unnest(stat_df)
 }
 
 
-## 3
+
+# 3
+select_markers_for_each_cell_type <- function(.data, sig_size) {
+  .data %>%
+    # split the contrast column into two so as to group all the contrasts into 4 groups based on cell_type
+    separate(col = contrast, into = c("contrast", "contrast2"), sep = "-", remove = T) %>% 
+    
+    # remove white spaces before names, make contrast easy to operate on
+    mutate(across(c("contrast", "contrast2"), ~ trimws(.x))) %>% 
+    
+    # group by cell_type, which is in the contrast column
+    nest(stat_df = -contrast) %>%
+    
+    # mutate(dup = map(stat_df, ~ .x %>% duplicated(symbol))) %>% 
+    # 
+    # mutate(stat_df = map(stat_df, ~ x. %>% subset(dup == T))) %>% 
+    
+    
+    # remove any duplicate genes within one cell_type that from different contrasts
+    mutate(stat_df = map(stat_df, ~ .x %>% dplyr::distinct(symbol, .keep_all = T))) %>% 
+    
+    mutate(stat_df = map(stat_df, ~ .x %>% slice(1:sig_size))) %>% 
+    
+    # mutate(stat_df = map(stat_df, ~ .x %>% nest(stat_df2 = -contrast2))) %>% 
+    
+    
+    unnest(stat_df)
+}
+
+
+## 4
 contrast <- function(tt){
   tt %>%
     
@@ -118,15 +157,17 @@ contrast <- function(tt){
       ~ test_differential_abundance(.x,
                                     ~ 0 + cell_type, 
                                     .contrasts = get_constrasts_from_df(.x),
-                                    action="only"
-      ) 
-      
-    )) %>%
+                                    action="only") 
+                        )
+          ) %>%
     
     # Select rank from each contrast
-    mutate(markers = map(markers, ~ select_markers_for_each_contrast(.x, sigsize))) %>%
+    mutate(markers = map(markers, ~ select_markers_for_each_contrast(.x, contrast_size))) %>%
     
-    # Add marker info to original data
+    # Select markers from each cell type
+    mutate(markers = map(markers, ~ select_markers_for_each_cell_type(.x, sig_size))) %>% 
+    
+    # Add original data info to markers
     mutate(markers = map2(markers, data, ~ left_join(.x, .y))) %>%
     select(markers) %>%
     unnest(markers) %>%
@@ -135,63 +176,80 @@ contrast <- function(tt){
     mutate(contrast_pretty = str_replace(contrast, "cell_type", "") %>% str_replace("cell_type", ""))
 }
 
-## 4 calculate the area of confidence ellipses and the sum of their areas
+## 5 calculate the area of confidence ellipses and the sum of their areas
 ellip_area <- function(all_contrasts){
+  # reduce dimension
   all_contrasts %>% 
     distinct(sample, symbol, count_scaled, cell_type) %>%
     nanny::reduce_dimensions(sample, symbol, count_scaled,  method = "PCA", action="get", transform = log1p) %>% 
+    
+    # remove non-numerical data to form a numerical dataframe
     select(-sample) %>% 
+    
+    # nest by cell_type so as to calculate ellipse area for each cell type
     nest(PC = -cell_type) %>% # pluck("PC", 1) %>% select(contains('PC')) %>% cov()
+    
+    # obtain covariance matrix for each cell type
     mutate(cov = map(PC, ~ cov(.x))) %>% # pluck("cov", 1)
+    
+    # calculate the eigenvalues for the covariance matrix of each cell type
     mutate(eigval = map(cov, ~ eigen(.x)$values)) %>% # pluck("eigval", 1)
+    
+    # transformation
     mutate(area = map(eigval, ~ sqrt(.x * qchisq(0.95, 2)))) %>% # unnest(area)
-    # one line below is the actual area, followed by log10 transformation to make values bigger
+    
+    # below is the actual area 
     mutate(area = map(area, ~ prod(.x)*pi)) %>% # unnest(area)
+    
+    # log10 transformation to make values bigger
     mutate(area = map_dbl(area, ~ 1/-log10(.x))) %>% 
+    
+    # summation of areas across all cell types
     pluck("area") %>% 
     sum()
 }
 
 
 # Ellipse area==================================================================================================
-sigsize = 17
-all_contrasts <- contrast(tt)
-
-
-PCA_level1 <- all_contrasts %>%
-  distinct(sample, symbol, count_scaled, cell_type) %>%
-  nanny::reduce_dimensions(sample, symbol, count_scaled,  method = "PCA", action="get", transform = log1p)
-
-PCA_level1
-
 n = 20
-area_sum <- 1:n
+area_sum <- 1: (contrast_size-1)
 
-for (i in 1:n) {
-  sigsize = i
+for (i in 1: (contrast_size-1)) {
+  sig_size = i + 1
  area_sum[i] <- tt %>% 
    contrast() %>% 
    ellip_area()
 }
 
-tb <- tibble(size = 1:n, total_area = area_sum)
+tb <- tibble(size = 2: contrast_size, total_area = area_sum)
 ggplot(tb, aes(size, total_area)) +
   geom_line() +
   geom_point()
 
-which.min(area_sum)  # 17 genes gives the minimum area_sum 
+which.min(area_sum)  # contrast_size=17 gives the minimum area_sum; 
+
+# sig_size=19 gives the minimum: 1.881965
+min(area_sum)
+
+contrast_size = 17
+contrast_size = 20
+sig_size = 19
 
 # Silhouette analysis=============================================================================
 
+# calculate the dissimilarity matrix
 distance <- PCA_level1 %>%
   select(contains("PC")) %>% 
   dist()
 
-
+# calculate silhouette score for each data point
 sil <- silhouette(as.numeric(PCA_level1$cell_type), distance)
 summary(sil)
 
+# plot all silhouette scores and store the data in sil_data
 sil_data <- fviz_silhouette(sil)
+
+# find the average silhouette width (or score) for all data points
 sil_score <- sil_data$data$sil_width %>% mean()
 
 sil_scores <- 1:n
@@ -201,35 +259,50 @@ neg_sil_index <- which(sil[, "sil_width"] < 0)
 neg_sil_index
 sil[neg_sil_index, , drop = FALSE]
 
-for (i in 1:n){
-  sigsize = i
+contrast_size = 60
+
+for (i in 1: (contrast_size-1)){
+  sig_size = i + 1
   all_contrasts <- contrast(tt)
+  
   PCA_level1 <- all_contrasts %>% 
     distinct(sample, symbol, count_scaled, cell_type) %>%
     nanny::reduce_dimensions(sample, symbol, count_scaled,  method = "PCA", action="get", transform = log1p)
+  
   distance <- PCA_level1 %>% 
     select(contains("PC")) %>% 
     dist()
+  
   sil <- silhouette(as.numeric(PCA_level1$cell_type), distance)
   sil_data <- fviz_silhouette(sil)
   sil_scores[i] <- sil_data$data$sil_width %>% mean()
 }
 
-scores <- 1:n
-for (i in 1:n){
+# convert scores from ggplot2 object into a vector
+scores <- 1: (contrast_size-1)
+
+for (i in 1:(contrast_size-1)){
   scores[i] <- sil_scores[[i]]
 }
-tb2 <- tibble(size = 1:n, scores = scores )
+
+tb2 <- tibble(size = 2:contrast_size, scores = scores )
 ggplot(tb2, aes(size, scores)) +
   geom_line() +
   geom_point()
 
 max(scores)
-which.max(scores) # 9 genes leads to maximum silhouette score
+which.max(scores) # contrast_size = 9 genes leads to maximum silhouette score; 
 
-sigsize = 9
+# sig_size = 7 (contrast_size = 20)gives maximum
+
+contrast_size = 9
 all_contrasts <- contrast(tt)
 
+contrast_size = 20
+sig_size = 7
+
+contrast_size = 9
+sig_size = 7
 
 # Plots==============================================================================================
 ## Plot Markers
@@ -246,6 +319,16 @@ all_contrasts %>%
 
 
 ## Plot PCA
+
+all_contrasts <- contrast(tt)
+
+
+PCA_level1 <- all_contrasts %>%
+  distinct(sample, symbol, count_scaled, cell_type) %>%
+  nanny::reduce_dimensions(sample, symbol, count_scaled,  method = "PCA", action="get", transform = log1p)
+
+PCA_level1
+
 PCA1 <- PCA_level1 %>% 
   ggplot(aes(x = PC1, y = PC2, colour = cell_type, label = sample)) + 
   geom_point() +
