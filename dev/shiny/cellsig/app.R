3157f9a6f0d23feb9d9acc431a9e18683d100864
jianwu1 <wujian0507.jw@gmail.com>
2021-01-20 20:54:13 +1100

    working app

--- dev/shiny/cellsig/app.R
+++ dev/shiny/cellsig/app.R
@@ -12,63 +12,79 @@ library(tidyverse)
 library(tidybulk)
 library(ggplot2)
 
-load("/stornext/Home/data/allstaff/w/wu.j/Master Project/cellsig/data/counts.rda")
-
-processed <- counts %>% 
-    tidybulk(sample, symbol, count) %>%
-    identify_abundant(factor_of_interest = cell_type) %>%
-    scale_abundance()
-    
-    select(-cell_type) %>% 
-    pivot_longer(contains("level"), names_to="level", values_to="cell_type") %>% 
-    filter(is.na(cell_type)==F & is.na(symbol)==F) %>% 
-    tidybulk(sample, symbol, count) %>%
-    nest(data = -level) %>%
-    mutate(data = map(data, ~ droplevels(.x))) %>%
-    # mutate(data = map(data, ~aggregate_duplicates(.x))) %>%
-    # mutate(data = map(data, ~ fill_missing_abundance(.x, fill_with = 0))) %>%
-    mutate(data = map(
-        data, ~ .x %>% 
-            identify_abundant(factor_of_interest = cell_type) %>%
-            scale_abundance(sample, symbol, count) 
-    ))
-
-
-# mutate(data = map(
-#     data, ~ .x %>% 
-#         identify_abundant(factor_of_interest = cell_type) %>%
-#         scale_abundance(.sample=sample, .transcript=symbol, .abundance=count) 
-# ))
+# load("/stornext/Home/data/allstaff/w/wu.j/Master Project/cellsig/dev/counts.rda")
+# 
+# processed <- counts %>%
+#     select(-cell_type) %>%
+#     pivot_longer(contains("level"), names_to="level", values_to="cell_type") %>%
+#     filter(is.na(cell_type)==F & is.na(symbol)==F) %>%
+#     tidybulk(sample, symbol, count) %>%
+#     nest(data = -level) %>%
+#     mutate(data = map(data, ~ droplevels(.x))) %>%
+#     # mutate(data = map(data, ~aggregate_duplicates(.x))) %>%
+#     # mutate(data = map(data, ~ fill_missing_abundance(.x, fill_with = 0))) %>%
+#     mutate(data = map(
+#         data, ~ .x %>%
+#             identify_abundant(factor_of_interest = cell_type) %>%
+#             scale_abundance(sample, symbol, count)
+#     ))
+# 
+# geneNames <- counts %>%
+#     select(symbol) %>%
+#     filter(is.na(symbol)==F) %>%
+#     distinct() %>%
+#     pull()
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Gene Expression Visualisation"),
+    titlePanel("Gene Expression Visualization"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
+        
         sidebarPanel(
             
             selectInput("gene",
                         h3("Enter a gene:"),
-                        choices = counts %>% select(symbol) %>% filter(is.na(symbol)==F) %>% distinct() %>% pull()
+                        choices = geneNames
             ),
             
             actionButton("submit",
-                         "Submit")
+                         "Submit"),
+            
+            width = 3
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("Level 1", plotOutput("geneExprL1")),
-                        tabPanel("Level 2", plotOutput("geneExprL2")),
-                        tabPanel("Level 3", plotOutput("geneExprL3")),
-                        tabPanel("Level 4", plotOutput("geneExprL4")),
-                        tabPanel("Level 5", plotOutput("geneExprL5"))
-            )
+            fluidRow(
+                h4("Level 1"),
+                plotOutput("geneExprL1")),
+            fluidRow(
+                h4("Level 2"),
+                plotOutput("geneExprL2")),
+            fluidRow(
+                h4("Level 3"),
+                plotOutput("geneExprL3")),
+            fluidRow(
+                h4("Level 4"),
+                plotOutput("geneExprL4")),
+            fluidRow(
+                h4("Level 5"),
+                plotOutput("geneExprL5")),
+            width = 9
+            
+            # tabsetPanel(type = "tabs",
+            #             tabPanel("Level 1", plotOutput("geneExprL1")),
+            #             tabPanel("Level 2", plotOutput("geneExprL2")),
+            #             tabPanel("Level 3", plotOutput("geneExprL3")),
+            #             tabPanel("Level 4", plotOutput("geneExprL4")),
+            #             tabPanel("Level 5", plotOutput("geneExprL5"))
+            # )
         )
     )
 )
@@ -78,92 +94,120 @@ server <- function(input, output) {
     
     gene <- reactive({input$gene})
     
+    plotData1 <- eventReactive(input$submit, {
+        processed %>%
+            filter(level=="level_1") %>%
+            unnest(data) %>%
+            filter(symbol==gene())
+    })
+    
+    plotData2 <- eventReactive(input$submit, {
+        processed %>%
+            filter(level=="level_2") %>%
+            unnest(data) %>%
+            filter(symbol==gene())
+    })
+    
+    plotData3 <- eventReactive(input$submit, {
+        processed %>%
+            filter(level=="level_3") %>%
+            unnest(data) %>%
+            filter(symbol==gene())
+    })
+    
+    plotData4 <- eventReactive(input$submit, {
+        processed %>%
+            filter(level=="level_4") %>%
+            unnest(data) %>%
+            filter(symbol==gene())
+    })
+    
+    plotData5 <- eventReactive(input$submit, {
+        processed %>%
+            filter(level=="level_5") %>%
+            unnest(data) %>%
+            filter(symbol==gene())
+    })
+    
     output$geneExprL1 <- renderPlot({
         
-        observeEvent(input$submit, {
-            tt %>%
-                filter(level=="level_1") %>%
-                unnest(data) %>%
-                filter(symbol==gene) %>%
-                ggplot(aes(cell_type, counts_scaled+1, color=cell_type)) +
-                scale_y_log10()+
-                geom_point() +
-                # facet_wrap(~sample) +
-                theme_bw() +
-                theme(text = element_text(size=6),
-                      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-        })
+        plotData1() %>% 
+            ggplot(aes(cell_type, count_scaled+1, color=cell_type)) +
+            labs(title=isolate({gene()}), y="Scaled count", color="Cell type") +
+            scale_y_log10() +
+            geom_violin() +
+            geom_jitter(alpha=0.3) +
+            theme_bw() +
+            theme(text = element_text(size=20),
+                  plot.title = element_text(hjust = 0.5),
+                  axis.title.x=element_blank(),
+                  axis.text.x = element_text(angle=45, vjust=1, hjust=1))
         
     })
     
     output$geneExprL2 <- renderPlot({
-            
-            observeEvent(input$submit, {
-                tt %>%
-                    filter(level==2) %>%
-                    unnest(data) %>%
-                    filter(symbol==gene) %>%
-                    ggplot(aes(cell_type, counts_scaled+1, color=cell_type)) +
-                    scale_y_log10()+
-                    geom_point() +
-                    # facet_wrap(~sample) +
-                    theme_bw() +
-                    theme(text = element_text(size=6),
-                          axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-            })
-        })
         
-    output$geneExprL3 <- renderPlot({
-            
-        observeEvent(input$submit, {
-            tt %>%
-                filter(level==3) %>%
-                unnest(data) %>%
-                filter(symbol==gene) %>%
-                ggplot(aes(cell_type, counts_scaled+1, color=cell_type)) +
-                scale_y_log10()+
-                geom_point() +
-                # facet_wrap(~sample) +
-                theme_bw() +
-                theme(text = element_text(size=6),
-                      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-            })
-        })
+        plotData2() %>% 
+            ggplot(aes(cell_type, count_scaled+1, color=cell_type)) +
+            labs(title=isolate({gene()}), y="Scaled count", color="Cell type") +
+            scale_y_log10() +
+            geom_violin() +
+            geom_jitter(alpha=0.3) +
+            theme_bw() +
+            theme(text = element_text(size=20),
+                  plot.title = element_text(hjust = 0.5),
+                  axis.title.x=element_blank(),
+                  axis.text.x = element_text(angle=45, vjust=1, hjust=1))
+        
+    })
 
+    output$geneExprL3 <- renderPlot({
+        
+        plotData3() %>% 
+            ggplot(aes(cell_type, count_scaled+1, color=cell_type)) +
+            labs(title=isolate({gene()}), y="Scaled count", color="Cell type") +
+            scale_y_log10() +
+            geom_violin() +
+            geom_jitter(alpha=0.3) +
+            theme_bw() +
+            theme(text = element_text(size=20),
+                  plot.title = element_text(hjust = 0.5),
+                  axis.title.x=element_blank(),
+                  axis.text.x = element_text(angle=45, vjust=1, hjust=1))
+        
+    })
+    
     output$geneExprL4 <- renderPlot({
         
-        observeEvent(input$submit, {
-            tt %>%
-                filter(level==4) %>%
-                unnest(data) %>%
-                filter(symbol==gene) %>%
-                ggplot(aes(cell_type, counts_scaled+1, color=cell_type)) +
-                scale_y_log10()+
-                geom_point() +
-                # facet_wrap(~sample) +
-                theme_bw() +
-                theme(text = element_text(size=6),
-                      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-        })
-    })
+        plotData4() %>% 
+            ggplot(aes(cell_type, count_scaled+1, color=cell_type)) +
+            labs(title=isolate({gene()}), y="Scaled count", color="Cell type") +
+            scale_y_log10() +
+            geom_violin() +
+            geom_jitter(alpha=0.3) +
+            theme_bw() +
+            theme(text = element_text(size=20),
+                  plot.title = element_text(hjust = 0.5),
+                  axis.title.x=element_blank(),
+                  axis.text.x = element_text(angle=45, vjust=1, hjust=1))
         
+    })
+    
     output$geneExprL5 <- renderPlot({
         
-        observeEvent(input$submit, {
-            tt %>%
-                filter(level==5) %>%
-                unnest(data) %>%
-                filter(symbol==gene) %>%
-                ggplot(aes(cell_type, counts_scaled+1, color=cell_type)) +
-                scale_y_log10()+
-                geom_point() +
-                # facet_wrap(~sample) +
-                theme_bw() +
-                theme(text = element_text(size=6),
-                      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-        })
+        plotData5() %>% 
+            ggplot(aes(cell_type, count_scaled+1, color=cell_type)) +
+            labs(title=isolate({gene()}), y="Scaled count", color="Cell type") +
+            scale_y_log10() +
+            geom_violin() +
+            geom_jitter(alpha=0.3) +
+            theme_bw() +
+            theme(text = element_text(size=20),
+                  plot.title = element_text(hjust = 0.5),
+                  axis.title.x=element_blank(),
+                  axis.text.x = element_text(angle=45, vjust=1, hjust=1))
+        
     })
-   
  }
 
 # Run the application 
