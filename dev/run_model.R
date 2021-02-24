7e587d308af17c3c99f5311f95072e544182fa67
stemangiola <mangiolastefano@gmail.com>
2021-02-25 10:23:22 +1100

    batchtools only on files

--- dev/run_model.R
+++ dev/run_model.R
@@ -12,44 +12,22 @@ slurm <- future::tweak(batchtools_slurm,
                        template = sprintf("%s/dev/modeling_files/slurm_batchtools.tmpl", local_dir),
                        resources=list(
                          cores = 1,
-                         memory_mb = 20000
+                         memory_mb = 10000,
+                         time = "12:00:00"
                        )
 )
 
 plan(slurm)
 
-# counts =
-#   readRDS(file="~/PhD/deconvolution/ARMET/dev/counts_infer_NB.rds") %>%
-#   #left_join( (.) %>% distinct(`symbol original`) %>% mutate(run = sample(1:5, n(), replace = T))) %>%
-#   #filter(`Cell type category` == "house_keeping" | run == my_run) %>%
-#   mutate(symbol = `symbol original`) %>%
-#
-#   # Replace the category house_keeping
-#   mutate(`house keeping` = `Cell type category` == "house_keeping") %>%
-#   rename(temp = `Cell type category`) %>%
-#   left_join(
-#     (.) %>%
-#       filter(temp != "house_keeping") %>%
-#       distinct(sample, temp, level) %>%
-#       rename(`Cell type category` = temp)
-#   ) %>%
-#   select(-temp) %>%
-#   filter(`Cell type category` %>% is.null %>% `!`) %>%
-#
-#   # Adapt it as input
-#   select(sample, symbol, count, `Cell type category`, level, `count scaled`, `house keeping`)
 
-
-# #
-load("dev/counts.rda")
 
 # Save files
 create_partition_files = function(.data, .level, .partitions = 30){
   .data %>%
-
+    
     # IMPORTANT! To change for every level
     mutate(level=.level, cell_type = !!as.symbol(sprintf("level_%s", .level))) %>%
-
+    
     # Process
     filter(cell_type %>% is.na %>% `!`) %>%
     nest(data = -c(cell_type, symbol)) %>%
@@ -60,12 +38,11 @@ create_partition_files = function(.data, .level, .partitions = 30){
     mutate(saved = map2_lgl(
       data, partition,
       ~ {
-        .x %>% saveRDS(sprintf("dev/modeling_files/level_%s_patition_%s.rds", .level, .y))
+        .x %>% saveRDS(sprintf("%s/dev/modeling_files/level_%s_patition_%s.rds", local_dir, .level, .y))
         TRUE
       }
     ))
 }
-
 
 create_partitions = function(.data, .level, .partitions = 30){
   .data %>%
@@ -81,37 +58,46 @@ create_partitions = function(.data, .level, .partitions = 30){
     nest(data = -partition)
 }
 
+# Create files
+# load("dev/counts.rda")
+# tibble(level=1:5) %>%
+#   mutate(partitions = map(level, ~ create_partition_files(counts, .x, 30))) 
 
-tibble(level=1:5) %>%
-  mutate(partitions = map(level, ~ create_partitions(counts, .x, 3000))) %>%
-  unnest(partitions) %>%
-
-  # FOR TESTING
-  slice(1:5) %>%
-  mutate(inference = future_imap(
-    data,
-    # ~ 5
+dir(sprintf("%s/dev/modeling_files/", local_dir), pattern = ".rds", full.names = T) %>%
+  enframe(value = "file") %>%
+  
+  mutate(inference = future_map(
+    file,
     ~ .x %>%
+      readRDS() %>%
       ref_intercept_only(
         exposure_rate,
         cores = 1,
         approximate_posterior = T
-    ),
+      ),
     .options = furrr_options(packages = c("tidyverse", "magrittr", "cellsig"))
   )) %>%
   saveRDS(sprintf("%s/dev/temp.rds", local_dir))
 
-# tibble(level=1) %>%
-#   mutate(partitions = map(level, ~ create_partitions(counts, .x, 3000))) %>%
-#   unnest(partitions) %>%
-#   
-#   # FOR TESTING
-#   slice(1) %>%
-#   pull(data) %>%
-#   .[[1]] %>%
-#   ref_intercept_only(
-#     exposure_rate,
-#     cores = 1,
-#     approximate_posterior = TRUE
-#   ) 
+
+# On the fly - PROBABLY TOO MUCH
+# counts_partitions =
+#   tibble(level=1:5) %>%
+#   mutate(partitions = map(level, ~ create_partition_files(counts, .x, 30))) %>%
+#   unnest(partitions)
+# 
+# counts_partitions %>%
+# 
+#   mutate(inference = future_map(
+#     data,
+#     ~ .x %>%
+#       ref_intercept_only(
+#         exposure_rate,
+#         cores = 1,
+#         approximate_posterior = T
+#     ),
+#     .options = furrr_options(packages = c("tidyverse", "magrittr", "cellsig"))
+#   )) %>%
+#   saveRDS(sprintf("%s/dev/temp.rds", local_dir))
+
 
