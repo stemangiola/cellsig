374b344c9723d62f4b5f87b355f8b354650ebd42
jianwu1 <wujian0507.jw@gmail.com>
2021-09-02 15:56:48 +1000

    subset tree functions added

--- dev/jian_R_files/function_jian.R
+++ dev/jian_R_files/function_jian.R
@@ -2544,3 +2544,64 @@ deconvolution_evaluation <- function(.signature, .mix, .preprocessed_non_hierarc
                 distinct(replicate, cell_type, proportion))
   
 }
+
+# subset tree
+
+tree_subset <- function(.tree, .node) {
+  
+  tree_subset_tibble(.tree = .tree, .node = .node) %>% 
+    
+    # reverse the row order of tibble to make the root node on top
+    # .[rev(1: nrow(.)), ] %>%
+    
+    update_tree_edge_matrix() %>% 
+    
+    as.phylo() %>% 
+    
+    as.Node()
+}
+
+tree_subset_tibble <- function(.tree, .node){
+  
+  # Args:
+  # .tree: a Node R6 object from data.tree
+  # .node: a character specifying the cell type of interest
+  
+  tree_tbl <- .tree %>% as.phylo %>% as_tibble
+  
+  if (.node == "Tissue") { return( tree_tbl %>% filter(label == "Tissue") )
+    
+  } else {
+    
+    return(sibling(tree_tbl, .node) %>% 
+             bind_rows( 
+               tree_subset_tibble(.tree, parent(tree_tbl, .node) %>% .$label),
+               .)
+    )
+  }
+  
+}
+
+update_tree_edge_matrix <- function(.tree_tbl) {
+  
+  # Args:
+  # .tree_tbl: a tree_tibble object from tidytree
+  
+  tree_phylo <- .tree_tbl %>% as.phylo
+  
+  .tree_tbl %>% 
+    
+    mutate(node = map_int(label, 
+                          ~ if(.x %in% tree_phylo$tip.label){
+                            which(.x == tree_phylo$tip.label)
+                          } else {
+                            which(.x == tree_phylo$node.label) + Ntip(tree_phylo)
+                          }
+    )) %>% 
+    
+    mutate(parent = rep(parent %>% unique() %>% rank(), 
+                        times = table(parent)) + Ntip(tree_phylo)
+    )
+  
+}
+
