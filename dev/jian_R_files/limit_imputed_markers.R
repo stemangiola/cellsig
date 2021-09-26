438d2674dab88d194ff8868386b41ed51d01dabf
jianwu1 <wujian0507.jw@gmail.com>
2021-09-26 23:09:15 +1000

    scaled_input_counts renamed as do_hierarchy

--- dev/jian_R_files/limit_imputed_markers.R
+++ dev/jian_R_files/limit_imputed_markers.R
@@ -43,61 +43,24 @@ preprocess <- function(.transcript, .level) {
 }
 
 readRDS("dev/intermediate_data/counts_imputed.rds") %>% 
-  scale_input_counts(.is_hierarchy = TRUE) %>% 
+  rename(symbol = feature) %>% 
+  do_hierarchy(.sample = sample, .symbol = symbol, .is_hierarchy = TRUE) %>% 
   saveRDS("dev/intermediate_data/counts_imputed_hierarchy.rds", compress = "xz")
 
 readRDS("dev/intermediate_data/counts_imputed.rds") %>% 
-  scale_input_counts(.is_hierarchy = FALSE) %>% 
+  rename(symbol = feature) %>% 
+  do_hierarchy(.sample = sample, .symbol = symbol, .is_hierarchy = FALSE) %>% 
   saveRDS("dev/intermediate_data/counts_imputed_non_hierarchy.rds", compress = "xz")
 
-preprocess <- function(.transcript, .level) {
-  
-  # load data
-  .transcript %>%
-    
-    dplyr::rename("symbol" = ".feature", sample = ".sample") %>% 
-    # tidybulk(sample, symbol, count_scaled) %>% for imputed counts data
-    # tidybulk(.sample = sample, .transcript = symbol, .abundance = count, .abundance_scaled = counts_scaled) %>%
-    
-    # filter for cells at the level of interest. .level == level_1
-    filter(is.na(!!as.symbol(.level))==FALSE) %>%
-    
-    nest(data = -c(symbol, !!as.symbol(.level))) %>%
-    
-    # for a cell type some samples may miss genes in other samples: so for the same cell type genes may have different number of samples
-    mutate(n_samples_per_gene = map_int(
-      data,
-      ~ .x %>%
-        distinct(sample) %>%
-        nrow)) %>%
-    
-    unnest(data) %>%
-    nest(data = -!!as.symbol(.level)) %>%
-    
-    mutate(n_samples= map_int(
-      data,
-      ~ .x %>%
-        distinct(sample) %>%
-        nrow
-    )) %>%
-    
-    unnest(data) %>% 
-    
-    mutate(ratio_imputed_samples = 1 - n_samples_per_gene / n_samples) %>% 
-
-    # nest by ancestor
-    nest(data = - !!as.symbol(pre(.level)))
-  
-}
 
 readRDS("dev/intermediate_data/counts_bayes_imputed.rds") %>% 
-  select(-level) %>% 
-  scale_input_counts(.is_hierarchy = TRUE) %>% 
+  rename(symbol = feature) %>% 
+  do_hierarchy(.sample = sample, .symbol = symbol, .is_hierarchy = TRUE) %>% 
   saveRDS("dev/intermediate_data/counts_bayes_imputed_hierarchy.rds", compress = "xz")
 
 readRDS("dev/intermediate_data/counts_bayes_imputed.rds") %>% 
-  select(-level) %>% 
-  scale_input_counts(.is_hierarchy = FALSE) %>% 
+  rename(symbol = feature) %>% 
+  do_hierarchy(.sample = sample, .symbol = symbol, .is_hierarchy = FALSE) %>% 
   saveRDS("dev/intermediate_data/counts_bayes_imputed_non_hierarchy.rds", compress = "xz")
 
 # ranked_MC_L4 <- tt_L4 %>%
