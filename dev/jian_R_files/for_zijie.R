961cd22eb3c7f7d566e4d65d7b95bda1888f68b9
jianwu1 <wujian0507.jw@gmail.com>
2021-10-06 20:54:37 +1100

    functions updated for zijie

--- dev/jian_R_files/for_zijie.R
+++ dev/jian_R_files/for_zijie.R
@@ -1,8 +1,82 @@
-# The results may take a few hours to produce so you do not necessarily need to run my example code
 
-# load all necessary functions
-
+# load all necessary functions YOU DO NOT WANT TO CHANGE DEFAULT parameter values for your own run ================
 source("dev/jian_R_files/function_jian.R")
+
+do_hierarchy <- function(.imputed_counts, .sample, .symbol, .cell_type, .tree, .is_hierarchy = TRUE, .level = NULL){
+  
+  .sample = enquo(.sample)
+  .symbol = enquo(.symbol)
+  .cell_type = enquo(.cell_type)
+  
+  if (.is_hierarchy) { # scaling under each ancestor node at each level
+    
+    if (is.null(.level)) { # scale counts for all levels present
+      
+      tibble(level = names(.imputed_counts) %>% str_subset("level")) %>%
+        
+        mutate(tt = map(level, ~ .imputed_counts %>%
+                          
+                          mutate(level_0 = "root") %>%
+                          
+                          create_hierarchy(.level=.x, .sample=!!.sample, .symbol=!!.symbol))) %>%
+        
+        mutate(tt = map2(tt, level, ~ .x %>% dplyr::rename(ancestor = pre(.y))))
+      
+    } else { # scale counts for the level specified by .level
+      
+      tibble(level = .level) %>%
+        
+        mutate(tt = map(level, ~ .imputed_counts %>%
+                          
+                          mutate(level_0 = "root") %>%
+                          
+                          create_hierarchy(.level=.x, .sample=!!.sample, .symbol=!!.symbol))) %>%
+        
+        mutate(tt = map2(tt, level, ~ .x %>% dplyr::rename(ancestor = pre(.y))))
+    }
+    
+  } else { # non-hierarchical: scaling all cell types under the root node
+    
+    .level <- "root"
+    
+    tibble(level = .level) %>%
+      
+      mutate(tt = map(level, ~ .imputed_counts %>%
+                        
+                        # non-hierarchical methods should only compare leaf cell types
+                        filter(!!.cell_type %in% as.phylo(.tree)$tips) %>% 
+                        
+                        # create a root column for pre(.level)
+                        mutate(!!as.symbol(.level) := !!.cell_type) %>%
+                        mutate(!!as.symbol(pre(.level)) := .level) %>%
+                        
+                        create_hierarchy(.level=.x, .sample=!!.sample, .symbol=!!.symbol))) %>%
+      
+      mutate(tt = map2(tt, level, ~ .x %>% dplyr::rename(ancestor = pre(.y))))
+    
+  }
+}
+
+create_hierarchy <- function(.imputed_counts, .level, .sample, .symbol) {
+  # this preproces function ranged data in hierarchy(or non_hierarchy) and
+  # calculates the imputation ratio for genes in each hierarchy
+  .sample = enquo(.sample)
+  .symbol = enquo(.symbol)
+  
+  
+  # load data
+  .imputed_counts %>%
+    
+    # tidybulk(sample, symbol, count_scaled) %>% for imputed counts data
+    # tidybulk(.sample = !!.sample, .transcript = !!.symbol, .abundance = !!.count) %>%
+    
+    # filter for cells at the level of interest. .level == level_1
+    filter(!is.na(!!as.symbol(.level))) %>%
+    
+    # nest by ancestor
+    nest(data = - !!as.symbol(pre(.level)))
+  
+}
 
 counts_tree_to_gene_markers = function(.input, .sample, .symbol, .count, .cell_type,
                                        .is_hierarchy=TRUE, .level=NULL, 
@@ -36,6 +110,7 @@ counts_tree_to_gene_markers = function(.input, .sample, .symbol, .count, .cell_t
       do_hierarchy(.sample=!!.sample,
                    .symbol=!!.symbol,
                    .cell_type = !!.cell_type,
+                   .tree = .tree,
                    .is_hierarchy=.is_hierarchy,
                    .level=.level) %>%
       
@@ -46,7 +121,8 @@ counts_tree_to_gene_markers = function(.input, .sample, .symbol, .count, .cell_t
                  .ranking_method=.ranking_method, 
                  .contrast_method=.contrast_method, 
                  .rank_stat=.rank_stat, 
-                 .bayes=.bayes) %>%  
+                 .bayes=.bayes,
+                 .tree = .tree) %>%  
       
       # Input: data.frame columns_1 <int> | ...
       # Output: 
@@ -79,6 +155,7 @@ counts_tree_to_gene_markers = function(.input, .sample, .symbol, .count, .cell_t
       do_hierarchy(.sample=!!.sample,
                    .symbol=!!.symbol,
                    .cell_type = !!.cell_type,
+                   .tree = .tree,
                    .is_hierarchy=.is_hierarchy,
                    .level=.level) %>%
       
@@ -87,7 +164,8 @@ counts_tree_to_gene_markers = function(.input, .sample, .symbol, .count, .cell_t
                  .ranking_method=.ranking_method, 
                  .contrast_method=.contrast_method, 
                  .rank_stat=.rank_stat, 
-                 .bayes=.bayes) %>% 
+                 .bayes=.bayes,
+                 .tree = .tree) %>% 
       
       mutate(level.copy = level) %>% 
       nest(data = -level.copy) %>% 
@@ -116,74 +194,14 @@ counts_tree_to_gene_markers = function(.input, .sample, .symbol, .count, .cell_t
   }
 }
 
-# load input data
 
+# load input data =============================================================================
+
+# MODIFY THIS: load your own tree
 tree = read_yaml("dev/tree.yaml") %>% as.Node
 
-# this is my example data
-counts_imputed <- 
-  readRDS("/stornext/Home/data/allstaff/w/wu.j/Master_Project/cellsig/dev/intermediate_data/counts_imputed.rds")
-
-# load your own input data
+# MODIFY THIS: load your own input data
 zijie_input_expression_data
-
-# Jian's example
-
-jian_example_output = 
-  
-  # input
-  counts_imputed %>% 
-  select(feature, sample, count_scaled, cell_type) %>% 
-  
-  # feed input into the function to obtain cell signatures
-  # I have set the parameters to the combination that gives good markers in terms of deconvolution score
-  counts_tree_to_gene_markers(
-    
-    # first specify the names of the essential variables(columns) in the input data frame so that the function knows which is which
-    .sample = sample, .symbol = feature, .cell_type = cell_type, .count = count_scaled,
-    
-    # choose hierarchical method: there are 2 options: hierarchical or non_hierarchical by setting .is_hierarchy to TRUE or FALSE
-    # hierarchical method is better for your purpose
-    .is_hierarchy=TRUE,
-    
-    # specify the tree structure
-    .tree = tree, 
-    
-    # specify the contrast method:
-    # there are 2 options: mean_contrast and pairwise_contrast
-    # choose pairwise_contrast for your purpose
-    # essentially to select markers we need to compare the differential expression between genes in different cell types
-    # say we have 2 cell types: t_CD4, t_CD8, b_cell
-    # pairwise_contrast generates 3*2=6 comparisons in the form t_CD4 - t_CD8, t_CD4 - b_cell, t_CD8 - t_CD4, t_CD8 - b_cell, b_cell - t_CD4, b_cell - t_CD8
-    # whereas mean_contrast generates 3 comparisons in the form of t_CD4 - (t_CD8 + b_cell)/2, t_CD8 - (t_CD4 + b_cell)/2, b_cell - (t_CD4 + t_CD8)/2
-    # the minus sign - doesn't mean subtraction here but only indicates which compares with which
-    # cell type before "-" is called the target cell whereas cell type(s) after the "-" is/are termed background cell type(s)
-    # say we have gene Myb, cell type contrast "t_CD4 - t_CD8" means that we compare Myb expression in t_CD4 with that in t_CD8
-    # whereas cell type contrast "t_CD4 - (t_CD8 + b_cell)/2" means we compare Myb expression in t_CD4 with the mean of Myb expression in t_CD8 and b_cell
-    .contrast_method = pairwise_contrast, 
-    
-    # specify ranking method:
-    # there are 3 options: rank_edgR_quasi_likelihood, rank_edgR_robust_likelihood_ratio, rank_bayes
-    # choose rank_edgR_quasi_likelihood
-    # for the ranking method rank_edgR_quasi_likelihood, there are 2 ranking statistic: "PValue" and "logFC"
-    # choose "PValue"
-    # we rank the genes for each cell type contrast by their level of differential expression
-    # the ones with lowest PValue or the highest logFC are ranked top 1 (most differentially expressed or most enriched in the target cell type)
-    .ranking_method = rank_edgR_quasi_likelihood, .rank_stat = "PValue",
-    
-    # specify marker selection method:
-    # there are 2 methods: "naive" and "silhouette"
-    # naive method selects the top k genes from the ranked list for each cell type contrast so that the markers are enriched 
-    # in the target cell type of each contrast
-    # I set the default value for k to be 60 which means 
-    # the top k genes (k belongs to [1, 60]) will be iteratively selected from each cell type contrast to make up the signature set for the target cell type
-    .selection_method = "naive",
-    
-    # specify optimisation method:
-    # there are 2 options: "curvature" and "penalty"
-    # out of 60 sets of signatures for each target cell type in a contrast which set performs the best?
-    # optimisation by "penalty" selects the best signature size based on a metric called penalised silhouette score
-    .optimisation_method = "penalty")
 
 
 # To select markers for your own data, run the code below:
@@ -214,3 +232,74 @@ zijie_input_expression_data %>%
   # save the output signature of the function
   # PLEASE MODIFY THIS LINE OF CODE
   saveRDS("your_directory/name_of_the_output_file.rds", compress = "xz")
+
+
+# Jian's example ====================================================================
+
+# this is my example data
+toy_data <- readRDS("/stornext/Home/data/allstaff/w/wu.j/Master_Project/cellsig/dev/intermediate_data/toy_data.rds")
+
+# create a small tree that fits the cell types in the toy data
+# cell types on the leaves of the tree must be present in your input data: such as epithelial t_cell, b_cell here
+# internal cell types such are immune_cell and Tissue here do not need to be in your input data
+jian_tree <- Node$new("Tissue")
+jian_tree$AddChild("epithelial")
+jian_tree$AddChild("immune_cell")
+jian_tree$immune_cell$AddChild("t_cell")
+jian_tree$immune_cell$AddChild("b_cell")
+
+
+jian_example_output = 
+  
+  # input
+  toy_data %>% 
+  
+  # feed input into the function to obtain cell signatures
+  # I have set the parameters to the combination that gives good markers in terms of deconvolution score
+  counts_tree_to_gene_markers(
+    
+    # first specify the names of the essential variables(columns) in the input data frame so that the function knows which is which
+    .sample = sample, .symbol = feature, .cell_type = cell_type, .count = count_scaled,
+    
+    # choose hierarchical method: there are 2 options: hierarchical or non_hierarchical by setting .is_hierarchy to TRUE or FALSE
+    # hierarchical method is better for your purpose
+    .is_hierarchy=TRUE,
+    
+    # specify the tree structure
+    .tree = jian_tree, 
+    
+    # specify the contrast method:
+    # there are 2 options: mean_contrast and pairwise_contrast
+    # choose pairwise_contrast for your purpose
+    # essentially to select markers we need to compare the differential expression between genes in different cell types
+    # say we have 2 cell types: t_CD4, t_CD8, b_cell
+    # pairwise_contrast generates 3*2=6 comparisons in the form t_CD4 - t_CD8, t_CD4 - b_cell, t_CD8 - t_CD4, t_CD8 - b_cell, b_cell - t_CD4, b_cell - t_CD8
+    # whereas mean_contrast generates 3 comparisons in the form of t_CD4 - (t_CD8 + b_cell)/2, t_CD8 - (t_CD4 + b_cell)/2, b_cell - (t_CD4 + t_CD8)/2
+    # the minus sign - doesn't mean subtraction here but only indicates which compares with which
+    # cell type before "-" is called the target cell whereas cell type(s) after the "-" is/are termed background cell type(s)
+    # say we have gene Myb, cell type contrast "t_CD4 - t_CD8" means that we compare Myb expression in t_CD4 with that in t_CD8
+    # whereas cell type contrast "t_CD4 - (t_CD8 + b_cell)/2" means we compare Myb expression in t_CD4 with the mean of Myb expression in t_CD8 and b_cell
+    .contrast_method = pairwise_contrast, 
+    
+    # specify ranking method:
+    # there are 3 options: rank_edgR_quasi_likelihood, rank_edgR_robust_likelihood_ratio, rank_bayes
+    # choose rank_edgR_quasi_likelihood
+    # for the ranking method rank_edgR_quasi_likelihood, there are 2 ranking statistic: "PValue" and "logFC"
+    # choose "PValue"
+    # we rank the genes for each cell type contrast by their level of differential expression
+    # the ones with lowest PValue or the highest logFC are ranked top 1 (most differentially expressed or most enriched in the target cell type)
+    .ranking_method = rank_edgR_quasi_likelihood, .rank_stat = "PValue",
+    
+    # specify marker selection method:
+    # there are 2 methods: "naive" and "silhouette"
+    # naive method selects the top k genes from the ranked list for each cell type contrast so that the markers are enriched 
+    # in the target cell type of each contrast
+    # I set the default value for k to be 60 which means (kmax set to 10 in this example so as not to waste your time)
+    # the top k genes (k belongs to [1, 60]) will be iteratively selected from each cell type contrast to make up the signature set for the target cell type
+    .selection_method = "naive", .kmax=10,
+    
+    # specify optimisation method:
+    # there are 2 options: "curvature" and "penalty"
+    # out of 60 sets of signatures for each target cell type in a contrast which set performs the best?
+    # optimisation by "penalty" selects the best signature size based on a metric called penalised silhouette score
+    .optimisation_method = "penalty")
