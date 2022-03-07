2f75cea62b4831289117783437dd435fa7b52e58
stemangiola <mangiolastefano@gmail.com>
2022-03-07 22:03:44 +1100

    update optimised more flexible model

--- tests/testthat/test-modelling.R
+++ tests/testthat/test-modelling.R
@@ -1,11 +1,19 @@
 test_that("model runs", {
 
-  readRDS("dev/modeling_results/level_1_cell_type_endothelial_partition_1_input.rds") %>% 
+  library(dplyr)
+  library(tidyr)
+  library(cellsig)
+  library(ggplot2)
+  
+  # debugonce(cellsig_multilevel_varing_intercept)
+  
+ x =
+   readRDS("dev/modeling_results/level_1_cell_type_endothelial_partition_1_input.rds") |> 
     
-    nest(data = -.feature) %>%
-    sample_n(50) %>%
-    unnest(data) %>%
+    nest(data = -.feature) |>
+    sample_n(200) |>
+    unnest(data) |>
     
-    mutate(multiplier = exp(exposure_rate)) %>% 
-    cellsig_multilevel_varing_intercept(.sample, .feature, count, cell_type, multiplier, database)
+    mutate(multiplier = exp(exposure_rate)) |> 
+    cellsig_multilevel_varing_intercept(.sample, .feature, count, cell_type, multiplier, database, pass_fit = TRUE)
 })
