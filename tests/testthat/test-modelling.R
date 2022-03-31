32c05bc0e3ca9e336972e223f61f8632f65eb790
stemangiola <mangiolastefano@gmail.com>
2022-04-01 02:40:57 +1100

    update tests

--- tests/testthat/test-modelling.R
+++ tests/testthat/test-modelling.R
@@ -1,19 +1,20 @@
 test_that("model runs", {
 
-  library(dplyr)
-  library(tidyr)
   library(cellsig)
-  library(ggplot2)
-  
+
   # debugonce(cellsig_multilevel_varing_intercept)
   
  x =
-   readRDS("dev/modeling_results/level_1_cell_type_endothelial_partition_1_input.rds") |> 
-    
-    nest(data = -.feature) |>
-    sample_n(50) |>
-    unnest(data) |>
+   test_random_intercept |>
     
     #mutate(multiplier = exp(exposure_rate)) |> 
-    cellsig_multilevel_varing_intercept(.sample, .feature, count, cell_type, multiplier, database, pass_fit = TRUE)
+    cellsig_multilevel_varing_intercept(
+      .sample, 
+      .feature,
+      count, 
+      cell_type,
+      multiplier, 
+      database, 
+      pass_fit = TRUE
+    )
 })
