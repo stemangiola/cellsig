4782a81c0627ad1b45b1a02123ac80df291da707
stemangiola <mangiolastefano@gmail.com>
2022-05-16 17:15:44 +1000

    slight update pipeline

--- dev/modeling_code/parse_results.R
+++ dev/modeling_code/parse_results.R
@@ -23,28 +23,42 @@ local_dir = "."
 
 library(data.tree)
 library(yaml)
-tree =  read_yaml("dev/tree_kamran.yaml") %>% as.Node
+
 
 # counts_bayes_OLD <- readRDS("~/counts_bayes.rds")
 # counts_bayes_new = counts_bayes %>% bind_rows(counts_bayes_OLD %>% anti_join(counts_bayes, by = c(".feature", "cell_type")))
+directory = glue("{local_dir}/dev/benchmark_database_crossvalidation/")
+prefix = "kamran_crossvalidation_dataset_1"
+
 
 counts_bayes = 
-  dir(sprintf("%s/dev/modeling_results/", local_dir), pattern = "result.rds", full.names = T) %>%
+  dir(directory, pattern = "result.rds", full.names = T) %>%
   future_map_dfr(~ {
     x = readRDS(.x) %>% mutate(file = .x)
     attr(x, "fit") = NULL
     attr(x, "rng") = NULL
     x
-  }) %>%
-  
+  }) 
+
+job::job({ counts_bayes %>% saveRDS(glue("{directory}/counts_bayes_{prefix}.rds"), compress = "xz") })
+
+counts_bayes_parsed = 
+  counts_bayes %>%
   
   #unite( "sample", c(cell_type, level), remove = FALSE) %>%
-  tree_and_signatures_to_database(tree, ., cell_type, cell_type, .feature,  `50%`)  
+  tree_and_signatures_to_database(
+    read_yaml("dev/tree_kamran.yaml") %>% as.Node, 
+    ., 
+    cell_type, 
+    cell_type, 
+    .feature,  
+    `50%`
+  )  
 
-job::job({ counts_bayes %>% saveRDS("dev/counts_bayes.rds", compress = "xz") })
+job::job({ counts_bayes_parsed %>% saveRDS(glue("{directory}/counts_bayes_{prefix}_parsed.rds"), compress = "xz") })
 
 counts_bayes_imputed = 
-  counts_bayes %>%
+  counts_bayes_parsed %>%
 
   # Convert to SE
   as_SummarizedExperiment(cell_type, .feature, c(`10%`, `50%`,  `90%`, log_mean, log_sd) ) %>%
@@ -60,6 +74,6 @@ counts_bayes_imputed =
   
   # Merge the imputed column
   mutate(.imputed = .imputed | .imputed.1 | .imputed.2 | .imputed.3  ) %>%
-  select(-c(  .imputed.1 , .imputed.2 ,.imputed.3 )) 
+  dplyr::select(-c(  .imputed.1 , .imputed.2 ,.imputed.3 )) 
 
- job::job({ saveRDS(counts_bayes_imputed, "dev/counts_bayes_imputed.rds", compress = "xz") })
+job::job({ counts_bayes_imputed %>% saveRDS(glue("{directory}/counts_bayes_{prefix}_parsed_imputed.rds"), compress = "xz") })
