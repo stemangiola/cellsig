347e916145ff3d7c0ac892b145be0db41eef1c02
stemangiola <mangiolastefano@gmail.com>
2022-09-07 10:22:52 +1000

    update simulation

--- dev/modeling_code/get_simulated_data_from_estimates.R
+++ dev/modeling_code/get_simulated_data_from_estimates.R
@@ -13,18 +13,31 @@ directory_path = args[1]
 
 output_path = glue("{directory_path}/simulated_data.rds")
 
+sample_cell_type_count = 
+  directory_path |> 
+  dir( pattern = "input.rds", full.names = TRUE) |> 
+  map_df(~ readRDS(.x) |> distinct(.sample, cell_type)) |> 
+  distinct(.sample, cell_type) |> 
+  count(cell_type)
 
-dir(directory_path, pattern = "result.rds", full.names = TRUE) |> 
+directory_path |> 
+  dir( pattern = "result.rds", full.names = TRUE) |> 
   map_df(~{
     result = readRDS(.x)
     
+    samples_to_sample =
+      sample_cell_type_count |> 
+      filter(cell_type == (pull(result, cell_type) |> unique() |> as.character())) |> 
+      pull(n)
+    
     attr(result, "indeces") |> 
       distinct(.feature, cell_type, feature_cell_type_idx) |> 
+      left_join(sample_cell_type_count, by = "cell_type") |> 
       left_join(
         
         attr(result, "rng")$draws("Y_gen") |> 
           as_draws_df() |> 
-          sample_n(30) |> 
+          sample_n(samples_to_sample, replace=TRUE) |> 
           rowid_to_column(var = ".sample") |> 
           tidyr::pivot_longer(-.sample) |> 
           mutate(value = as.integer(value)) |> 
@@ -37,5 +50,6 @@ dir(directory_path, pattern = "result.rds", full.names = TRUE) |>
   select(-feature_cell_type_idx) |> 
   mutate_if(is.character, as.factor) |> 
   saveRDS(output_path, compress = "xz")
+
 
 
