0db62ee67d8e0d6745cab86511f69a28735b71cc
XpelC <chen.x@rstudio-1.hpc.wehi.edu.au>
2022-09-29 10:37:12 +1000

    commit parsing script we don't know where it came from

--- dev/analysis_prostate_atlas/parse_GSE120716.R
+++ dev/analysis_prostate_atlas/parse_GSE120716.R
@@ -5,7 +5,7 @@ library(SeuratDisk)
 library(scater)
 
 library(zellkonverter)
-GSE120716 = readH5AD(  file = "PPCG_deconvolution_signatures_RAW_DATA/GSE120716/henry18_0.processed.h5ad")
+GSE120716 = readH5AD(file = "PPCG_deconvolution_signatures_RAW_DATA/GSE120716/henry18_0.processed.h5ad")
 
 names(assays(GSE120716)) = "counts"
 GSE120716 %>% 
